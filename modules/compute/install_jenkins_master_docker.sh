234bc31a57eb2038c44533a5ccf8b1913a1d50b9
JBetoReyes <josereyes55@hotmail.com>
2022-08-21 23:02:11 -0500

    passes node ips to docker container

--- modules/compute/install_jenkins_master_docker.sh
+++ modules/compute/install_jenkins_master_docker.sh
@@ -4,6 +4,7 @@ echo "bucket name"
 echo "${bucket_name}"
 
 EC2_HOME=/home/ec2-user
+EC2_IP=$(wget -q -O -  http://169.254.169.254/latest/meta-data/public-ipv4)
 sudo yum update –y
 
 yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
@@ -23,7 +24,7 @@ systemctl start docker.service
 mkdir $EC2_HOME/jenkins_home $EC2_HOME/.ssh
 cd $EC2_HOME
 
-# Configuration as code file for jenkins
+# Nodes private key
 aws s3api get-object --bucket ${bucket_name} --key keys/id_rsa .ssh/id_rsa
 # restrict the access to keys
 chmod 700 $EC2_HOME/.ssh
@@ -45,4 +46,11 @@ pwd | echo
 
 docker build -t jenkins:wizardless .
 
-docker run --rm -p 8080:8080  --env JENKINS_ADMIN_ID=beto --env JENKINS_ADMIN_PASSWORD=admin2342 -v $EC2_HOME/jenkins_home:/var/jenkins_home --name jenkins jenkins:wizardless
+docker run --rm -p 8080:8080  \
+    --env JENKINS_ADMIN_ID=beto \
+    --env JENKINS_ADMIN_PASSWORD=admin2342 \
+    --env NODE_ONE_IP=${node_one_ip} \
+    --env NODE_TWO_IP=${node_two_ip} \
+    --env JENKINS_IP=$EC2_IP \
+    -v $EC2_HOME/jenkins_home:/var/jenkins_home \
+    --name jenkins jenkins:wizardless
