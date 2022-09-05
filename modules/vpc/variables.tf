3acb7cbe0bf8be49971bafb7b3601c4645ead7c7
JBetoReyes <josereyes55@hotmail.com>
2022-09-05 17:38:54 -0500

    adds variables

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,9 +1,21 @@
 variable "vpc_cidr_block" {
-    description = "vpc CIDR block"
-    default = "10.0.0.0/16"
+  description = "vpc CIDR block"
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr_block" {
-    description = "vpc CIDR block"
-    default = "10.0.0.0/24"
+  description = "vpc CIDR block"
+  default     = "10.0.0.0/24"
 }
+
+variable "private_eks_subnets_cidr_block" {
+  type    = list(string)
+  default = ["10.0.10.0/24", "10.0.12.0/24"]
+}
+
+variable "public_eks_subnets_cidr_block" {
+  type    = list(string)
+  default = ["10.0.13.0/24", "10.0.14.0/24"]
+}
+
+variable "cluster_name" {}
