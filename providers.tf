613033e75e1e40e5a001a9d5a875552983bd5d1f
JBetoReyes <josereyes55@hotmail.com>
2022-09-05 17:40:15 -0500

    sets a tf backend

--- providers.tf
+++ providers.tf
@@ -1,3 +1,19 @@
 provider "aws" {
   region = "us-west-1"
 }
+
+terraform {
+
+  required_providers {
+    aws = {}
+  }
+
+  required_version = "1.2.7"
+
+  backend "s3" {
+    bucket               = "jenkins-tf-beto-2022"
+    key                  = "infra.json"
+    region               = "us-west-1"
+    workspace_key_prefix = "environment"
+  }
+}
