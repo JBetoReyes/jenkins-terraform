5fba376093e413615384b758bdc38994e7a8211b
JBetoReyes <josereyes55@hotmail.com>
2022-09-05 17:17:35 -0500

    tf format

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ module "vpc" {
 }
 
 module "security_group" {
-   source = "./modules/security_group"
-   vpc_id = module.vpc.vpc_id
+  source = "./modules/security_group"
+  vpc_id = module.vpc.vpc_id
 }
 
 module "bucket" {
