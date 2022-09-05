2940e95732fced9258ad43878090d5f089ad23f6
JBetoReyes <josereyes55@hotmail.com>
2022-09-05 17:29:21 -0500

    moves SG to a new file

---
+++ modules/vpc/security_groups.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "eks-cluster" {
+  name   = "SG-eks-cluster"
+  vpc_id = aws_vpc.jenkins_vpc.id
+
+  # Egress allows Outbound traffic from the EKS cluster to the  Internet 
+
+  egress { # Outbound Rule
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Ingress allows Inbound traffic to EKS cluster from the  Internet 
+
+  ingress { # Inbound Rule
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
