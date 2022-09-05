01453f11e7410d29ac81ff0dd3a51da79b593f43
JBetoReyes <josereyes55@hotmail.com>
2022-09-05 17:32:58 -0500

    adding outputs to vpc module

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,9 +1,19 @@
 output "public_subnet_id" {
-    description = "ID of the public subnet"
-    value = aws_subnet.public_subnet.id
+  description = "ID of the public subnet"
+  value       = aws_subnet.public_subnet.id
+}
+
+output "kubernetes_public_subnet_ids" {
+  description = "IDs of the k8s public subnets"
+  value       = aws_subnet.kubernetes_public_subnets.*.id
+}
+
+output "kubernetes_private_subnet_ids" {
+  description = "IDs of the k8s private subnets"
+  value       = aws_subnet.kubernetes_private_subnets.*.id
 }
 
 output "vpc_id" {
-    description = "ID of our VPC"
-    value = aws_vpc.jenkins_vpc.id
+  description = "ID of our VPC"
+  value       = aws_vpc.jenkins_vpc.id
 }
